6f7747031ee8e7d147667696ee825b11d4a482f3
WebCheng <webster0808web@gmail.com>
2020-02-04 21:50:47 -0800

    Add mongodb

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(routes);
 
+
 const server = app.listen(8080, function () {
+
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://root:password@localhost:27017/";
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.createCollection("customers", function(err, res) {
+            if (err) throw err;
+            console.log("Collection created!");
+            db.close();
+        });
+    });
+
+    console.log("Example app listening ");
     let port = server.address().port;
     console.log("Example app listening at http://localhost:%s", port);
 });
